3a3dc59efa937138522a66b7d66147ed1cad9f76
GabrielleDepoli <gabriellefernandesdepoli@gmail.com>
2025-04-10 16:33:50 -0300

    Add UI

--- Ola.java
+++ Ola.java
@@ -1,5 +1,7 @@
+import javax.swing.JOptionPane;
 public class Ola{
     public static void main(String[] args){
-        System.out.println("Boa tarde a todos!");
+       //System.out.println("Boa tarde a todos!");
+       JOptionPane.showMessageDialog(null, "Boa tarde a todos!");
     }
 }
